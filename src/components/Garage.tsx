32e7c63083264524b190d7dddf5cba55aa4f140c
narekyan <narekb91@gmail.com>
2024-03-30 10:07:27 +0400

    feat: add winning page refactor: garage view

--- src/components/Garage.tsx
+++ src/components/Garage.tsx
@@ -1,83 +1,73 @@
 import React, { useState, useEffect } from 'react';
-import './Garage.css'; // Import CSS for styling
+import { useNavigate } from 'react-router-dom';
+import './Garage.css';
 
 const Garage: React.FC = () => {
-    const [cars, setCars] = useState<any[]>([]); // State to store cars
-    const [currentPage, setCurrentPage] = useState<number>(1); // State to track current page
-    const [viewName, setViewName] = useState<string>('Garage'); // Name of the current view
+    const [cars, setCars] = useState<any[]>([]);
+    const navigate = useNavigate();
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [viewName, setViewName] = useState<string>('Garage');
     const [velocity, setVelocity] = useState<{ [carId: number]: number }>({});
     const [translateValue, setTranslateValue] = useState<{ [carId: number]: number }>({});
     const [isAnimating, setIsAnimating] = useState<{ [carId: number]: boolean }>({});
     const [totalCars, setTotalCars] = useState<number>(0);
-    const [pageSize] = useState(7); // Number of cars per page
+    const [pageSize] = useState(7);
     const [isRacing, setIsRacing] = useState(false);
-    const [winner, setWinner] = useState<number>(0); // State to track current page
+    const [winner, setWinner] = useState<number>(0);
 
-    // Function to fetch cars for current page
+
     const fetchCarsForPage = async (page: number) => {
         const response = await fetch(`http://localhost:3000/garage?_page=${page}&_limit=7`);
         const data = await response.json();
         setTotalCars(getTotalCars(response));
-        if (data) { // Check if data and data.cars are not null
+        if (data) {
             setCars(data);
         }
 
     };
 
     const handleRaceClick = async () => {
-        // Start the race for all cars
-        // Implement this logic according to your application's requirements
 
         for (const car of cars) {
-            await handleStartCar(car.id); // Call handleStartCar for each car
+            await handleStartCar(car.id);
         }
 
         for (const car of cars) {
-
-
             const element = document.getElementById(`car${car.id}`);
             element?.addEventListener("transitionend", () => {
-                // This function will be called when the transition ends
                 if (winner == 0) {
                     setWinner(car.id)
                     alert(`Car ${car.name} won!`)
-                    postWinner({ id: car.id, wins: 1, time: 10 });
+
+                    navigate(`/winners`)
                     handleResetClick()
                 }
             });
 
-            const newTranslateValue = window.innerWidth; // Set it to the width of the viewport
+            const newTranslateValue = window.innerWidth;
             setTranslateValue((prevState) => ({
                 ...prevState,
-                [car.id]: newTranslateValue - 82, // Set animation status for this car to true
+                [car.id]: newTranslateValue - 82,
             }));
         }
-
-
-        console.log('Starting the race for all cars...');
-
-
-
-
         setIsRacing(true);
     };
 
+    // specific car start animation 
     const handleStartClick = async (carId: number) => {
-        await handleStartCar(carId); // Call handleStartCar for each car    
-        const newTranslateValue = window.innerWidth; // Set it to the width of the viewport
+        await handleStartCar(carId);
 
+        const newTranslateValue = window.innerWidth;
         setTranslateValue((prevState) => ({
             ...prevState,
-            [carId]: newTranslateValue - 82, // Set animation status for this car to true
+            [carId]: newTranslateValue - 82,
         }));
     }
 
+    // stop tthe race
     const handleResetClick = async () => {
-        // Start the race for all cars
-        // Implement this logic according to your application's requirements
-
         for (const car of cars) {
-            await handleStopCar(car.id); // Call handleStartCar for each car
+            await handleStopCar(car.id);
         }
         console.log('Starting the race for all cars...');
         setIsRacing(false);
@@ -88,7 +78,7 @@ const Garage: React.FC = () => {
         const totalCountHeader = response.headers.get('X-Total-Count');
         return totalCountHeader ? parseInt(totalCountHeader, 10) : 0;
     };
-    // Function to handle pagination
+
     const handleNextPage = () => {
         setCurrentPage((prevPage) => prevPage + 1);
     };
@@ -97,9 +87,8 @@ const Garage: React.FC = () => {
         setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
     };
 
-    // Function to generate random cars
     const generateRandomCars = async () => {
-        // Assuming there's an API endpoint for generating random cars
+
         for (let i = 0; i < 100; i++) {
             await postCar({ name: `Car ${i}`, color: getRandomColor() });
         }
@@ -110,36 +99,23 @@ const Garage: React.FC = () => {
         return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
     };
 
-    const postCar = async (car: { name: string; color: string }) => {
-        await fetch('http://localhost:3000/garage', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(car),
-        });
-    };
-
-
     const handleNameChange = (index: number, newName: string) => {
         setCars((prevCars) =>
             prevCars.map((car, i) => (i === index ? { ...car, name: newName } : car))
         );
     };
 
-    // Function to handle changing the color of a car
     const handleColorChange = (index: number, newColor: string) => {
         setCars((prevCars) =>
             prevCars.map((car, i) => (i === index ? { ...car, color: newColor } : car))
         );
     };
 
-
-    // Render cars
+    // render cars list
     const renderCars = () => {
         return cars.map((car: any, index: number) => (
             <div key={index} className="car" style={{ marginBottom: '10px' }}>
-                {/* Editable input field for the car name */}
+                {/* buttons */}
                 <div className='name-color' style={{ marginBottom: '5px', display: isRacing ? 'none' : 'block' }}>
                     <input
                         type="text"
@@ -156,18 +132,13 @@ const Garage: React.FC = () => {
                     />
                     <button style={{ marginRight: '5px' }} onClick={() => handleUpdateCar(car.id, car.name, car.color)}>Update</button>
 
-                    {/* Button to delete the car */}
                     <button onClick={() => handleDeleteCar(car.id)}>Delete</button>
                 </div>
-                {/* Color preview and selection */}
-                {/* Buttons to update and delete the car */}
                 <div style={{ marginBottom: '5px', display: isRacing ? 'none' : 'block' }}>
-                    {/* Button to update the car */}
                     <button style={{ marginRight: '5px' }} disabled={isAnimating[car.id]} onClick={() => handleStartClick(car.id)}>Start</button>
-                    {/* Button to delete the car */}
                     <button disabled={!isAnimating[car.id]} onClick={() => handleStopCar(car.id)}>Stop</button>
                 </div>
-                {/* Car icon */}
+                {/* car icon */}
                 <div className='car-cont' style={{ width: '100%' }}>
                     <div id={`car${car.id}`} className="car-container" style={{ paddingTop: '3px', paddingLeft: '2px' }}>
                         <img
@@ -189,6 +160,15 @@ const Garage: React.FC = () => {
         ));
     };
 
+    const postCar = async (car: { name: string; color: string }) => {
+        await fetch('http://localhost:3000/garage', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(car),
+        });
+    };
 
 
 
@@ -223,7 +203,6 @@ const Garage: React.FC = () => {
                 method: 'DELETE',
             });
             if (response.ok) {
-                // Car successfully deleted, perform any necessary actions (e.g., update state)
                 setCars(prevCars => prevCars.filter(car => car.id !== carId));
             } else {
                 // Handle error, show error message to the user
@@ -235,6 +214,7 @@ const Garage: React.FC = () => {
         }
     };
 
+    //specific cat start
     const handleStartCar = async (carId: number) => {
         try {
             const response = await fetch(`http://localhost:3000/engine?id=${carId}&status=started`, {
@@ -247,17 +227,18 @@ const Garage: React.FC = () => {
 
             setVelocity((prevState) => ({
                 ...prevState,
-                [carId]: data.velocity, // Set animation status for this car to true
+                [carId]: data.velocity,
             }));
             setIsAnimating((prevState) => ({
                 ...prevState,
-                [carId]: true, // Set animation status for this car to true
+                [carId]: true,
             }));
         } catch (error) {
             console.error('Error starting car:', error);
         }
     }
 
+    //specific car stop
     const handleStopCar = async (carId: number) => {
         try {
             const response = await fetch(`http://localhost:3000/engine?id=${carId}&status=stopped`, {
@@ -268,15 +249,15 @@ const Garage: React.FC = () => {
             });
             setTranslateValue((prevState) => ({
                 ...prevState,
-                [carId]: 0, // Set animation status for this car to true
+                [carId]: 0,
             }));
             setVelocity((prevState) => ({
                 ...prevState,
-                [carId]: 0, // Set animation status for this car to true
+                [carId]: 0,
             }));
             setIsAnimating((prevState) => ({
                 ...prevState,
-                [carId]: false, // Set animation status for this car to true
+                [carId]: false,
             }));
         } catch (error) {
             console.error('Error stopping car:', error);
@@ -298,8 +279,8 @@ const Garage: React.FC = () => {
                 <button onClick={generateRandomCars}>Generate Random Cars</button>
             </div>
             <div className="car-list">{renderCars()}</div>
+            {/* pagination */}
             <div>
-                {/* Pagination controls */}
                 <button style={{ marginRight: '5px' }} onClick={handlePrevPage} disabled={currentPage === 1}>
                     Previous
                 </button>
--- src/components/Winners.tsx
+++ src/components/Winners.tsx
@@ -2,8 +2,21 @@ import React, { useState, useEffect } from 'react';
 
 
 const Winners: React.FC = () => {
-    const [winners, setWinners] = useState<any[]>([]); // State to store cars
+    const [winners, setWinners] = useState<any[]>([]);
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [viewName, setViewName] = useState<string>('Garage');
+    const [totalWinners, setTotalWinners] = useState<number>(0);
+    const [pageSize] = useState(7);
 
+    const fetchWinnersForPage = async (page: number) => {
+        const response = await fetch(`http://localhost:3000/winners?_page=${page}&_limit=7`);
+        const data = await response.json();
+        setTotalWinners(getTotalWinners(response));
+        if (data) {
+            setWinners(data);
+        }
+
+    };
 
     const postWinner = async (winner: { id: number; wins: number; time: number }) => {
         await fetch('http://localhost:3000/winner', {
@@ -15,11 +28,57 @@ const Winners: React.FC = () => {
         });
     }
 
+    const getTotalWinners = (response: Response) => {
+        const totalCountHeader = response.headers.get('X-Total-Count');
+        return totalCountHeader ? parseInt(totalCountHeader, 10) : 0;
+    };
+
+    const handleNextPage = () => {
+        setCurrentPage((prevPage) => prevPage + 1);
+    };
+
+    const handlePrevPage = () => {
+        setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+    };
+
+
+    const renderWinners = () => {
+        return winners.map((winner: any, index: number) => (
+            <div key={index} className="winner" style={{ marginBottom: '10px' }}>
+
+                <div className='winner-cont' style={{ width: '100%' }}>
+                    <div id={`winner${winner.id}`} className="winner-container" style={{ paddingTop: '3px', paddingLeft: '2px' }}>
+                        {winner.id}
+                    </div>
+
+                </div>
+            </div >
+        ));
+    };
+
+    useEffect(() => {
+        fetchWinnersForPage(currentPage);
+
+        postWinner({ id: 2, wins: 1, time: 10 });
+
+
+    }, [currentPage]);
+
+
 
     return (
         <div>
             <h2>Winners</h2>
-            <p>This is the Winners view.</p>
+            <div className="winners-list">{renderWinners()}</div>
+            <div>
+                <button style={{ marginRight: '5px' }} onClick={handlePrevPage} disabled={currentPage === 1}>
+                    Previous
+                </button>
+                {currentPage}
+                <button style={{ marginLeft: '5px' }} onClick={handleNextPage} disabled={currentPage * pageSize >= totalWinners}>
+                    Next
+                </button>
+            </div>
         </div>
     );
 };
